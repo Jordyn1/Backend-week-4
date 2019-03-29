5d5187bbbbdce048a74e59d5f569abb9c35e8829
jordyn <jordyn.kerrison@live.com.au>
2019-03-29 23:09:20 +1100

    Create config.php

---
+++ public/config.php
@@ -0,0 +1,12 @@
+
+<?php
+    $host = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "jordyn_week4";
+    $dsn = "mysql:host=$host;dbname=$dbname";
+    $options = array (
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    )
+       
+?>
