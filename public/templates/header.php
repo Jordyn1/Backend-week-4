a3cdb64dd5dadc0b9fef9f36aa1811bed35777a4
jordyn <jordyn.kerrison@live.com.au>
2019-03-29 23:09:42 +1100

    Create header.php

---
+++ public/templates/header.php
@@ -0,0 +1,24 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<meta charset="utf-8">
+<link href="/Backend-week-4/public/assets/css/styles.css" rel="stylesheet" type="text/css" >  
+    
+    <head>
+        <title> Jordyn's Movie Tracker</title>        
+    </head>
+    
+    <body>
+        <div class="wrapper">
+     
+   
