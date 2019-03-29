4a326e70b5741313f3400a09480773589e70fbdb
Jordyn1 <47766993+Jordyn1@users.noreply.github.com>
2019-03-29 23:08:44 +1100

    Delete common.php

--- common.php
+++
@@ -1,7 +0,0 @@
-<?php
-/** 
-    This file holds commonly used functions * Remember, keep the code DRY! 
-**/
-
-
- function escape($html) { return htmlspecialchars($html, ENT_QUOTES | ENT_SUBSTITUTE, "UTF-8"); }?>
