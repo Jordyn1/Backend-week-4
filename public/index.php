93b4112eefaba1b236ee619ff12a2b2753b51166
jordyn <jordyn.kerrison@live.com.au>
2019-03-29 23:09:29 +1100

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,25 @@
+
+<!doctype html>
+<html lang="en">
+<meta charset="utf-8">
+<link href="/Backend-week-4/public/assets/css/styles.css" rel="stylesheet" type="text/css" >  
+   
+    <head>
+        <title> Jordyn's Movie Tracker</title>
+        
+    </head>
+    
+    <body>
+        
+
+<h3>Movie Tracker</h3>
+   
+
+    <ul>
+        <li><a href="login.php" class="index-menu">Log In</a></li>
+        <li><a href="register.php" class="index-menu">Register</a></li>
+
+    </ul>
+ 
+
+<?php include "templates/footer.php"?>
