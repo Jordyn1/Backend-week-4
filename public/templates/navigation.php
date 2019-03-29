098dcc64af80c0389bb6187a7d0dcd8f1bfac3ad
jordyn <jordyn.kerrison@live.com.au>
2019-03-29 23:09:44 +1100

    Create navigation.php

---
+++ public/templates/navigation.php
@@ -0,0 +1,13 @@
+   <header class="navigation-header">
+            <h1><a href="home.php">Movie Tracker</a></h1>
+ 
+    
+            <ul>
+                
+                <li><a href="create.php">Add a Movie</a></li>
+                <li><a href="read.php">View all</a></li>
+                <li><a href="logout.php">Log Out</a></li>
+              
+            </ul>
+       <hr class="navigation-break">
+    </header>
