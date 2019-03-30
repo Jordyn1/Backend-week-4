446e4143e6718f2305f18bced4c1bdaf225b47a8
Jordyn1 <47766993+Jordyn1@users.noreply.github.com>
2019-03-30 11:22:01 +1100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+
+    $host = "sql110.epizy.com";
+    $username = "epiz_23680553";
+    $password = "ev7crvPLVq75o";
+    $dbname = "epiz_23680553_movie_tracker";
+    $dsn = "mysql:host=$host;dbname=$dbname";
+    $options = array (
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    )
+   
+
+
+        
+?>
